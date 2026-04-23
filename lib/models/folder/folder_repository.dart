78f445216cf7e8c41b52ba88d3aa0b9fcba5b1e4
David Leonardo <dlpuerta@gmail.com>
2026-04-22 22:03:25 -0500

    models

--- lib/models/folder/folder_repository.dart
+++ lib/models/folder/folder_repository.dart
@@ -1,1 +1,9 @@
-// Abstract interface for folder persistence
+import 'package:notes/models/folder/folder.dart';
+
+abstract class FolderRepository {
+  Future<void> insert(Folder folder);
+  Future<Folder?> findById(String id);
+  Future<List<Folder>> findAll();
+  Future<void> update(Folder folder);
+  Future<void> delete(String id);
+}
--- lib/models/note/note_repository.dart
+++ lib/models/note/note_repository.dart
@@ -1,1 +1,13 @@
-// Abstract interface for note persistence
+import 'package:notes/models/note/note.dart';
+
+abstract class NoteRepository {
+  Future<void> insert(Note note);
+  Future<Note?> findById(String id);
+  Future<List<Note>> findAll();
+  Future<List<Note>> findByFolderId(String folderId);
+  Future<List<Note>> searchByTitle(String query);
+  Future<void> update(Note note);
+  Future<void> delete(String id);
+  Future<void> deleteAllInFolder(String folderId);
+  Future<void> moveAllToFolder(List<String> ids, String targetFolderId);
+}
