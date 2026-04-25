34a0a867951d3aefb3db8264c8ecc0b1892751f1
David Leonardo <dlpuerta@gmail.com>
2026-04-24 21:58:16 -0500

    search

--- lib/presentation/search/providers/search_provider.dart
+++ lib/presentation/search/providers/search_provider.dart
@@ -1,1 +1,31 @@
-// Riverpod providers for search state
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:notes/models/note/note.dart';
+import 'package:notes/models/note/use_cases/search_notes_by_title.dart';
+import 'package:notes/presentation/notes/providers/note_provider.dart';
+
+final _searchNotesByTitleProvider = Provider<SearchNotesByTitle>((ref) {
+  return SearchNotesByTitle(ref.read(noteRepositoryProvider));
+});
+
+class SearchState {
+  const SearchState({this.query = '', this.results = const []});
+
+  final String query;
+  final List<Note> results;
+}
+
+class SearchNotifier extends Notifier<SearchState> {
+  @override
+  SearchState build() => const SearchState();
+
+  Future<void> setQuery(String query) async {
+    final results =
+        await ref.read(_searchNotesByTitleProvider).execute(query);
+    state = SearchState(query: query, results: results);
+  }
+
+  void clear() => state = const SearchState();
+}
+
+final searchProvider =
+    NotifierProvider<SearchNotifier, SearchState>(SearchNotifier.new);
--- lib/presentation/search/widgets/search_bar.dart
+++ lib/presentation/search/widgets/search_bar.dart
@@ -1,1 +1,37 @@
-// Widget: search input field
+import 'package:flutter/material.dart';
+
+class NoteSearchBar extends StatefulWidget {
+  const NoteSearchBar({super.key, required this.onChanged});
+
+  final void Function(String query) onChanged;
+
+  @override
+  State<NoteSearchBar> createState() => _NoteSearchBarState();
+}
+
+class _NoteSearchBarState extends State<NoteSearchBar> {
+  final _controller = TextEditingController();
+  final _focusNode = FocusNode();
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    _focusNode.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      key: const Key('search_field'),
+      controller: _controller,
+      focusNode: _focusNode,
+      autofocus: true,
+      decoration: const InputDecoration(
+        hintText: 'Search notes...',
+        prefixIcon: Icon(Icons.search),
+      ),
+      onChanged: widget.onChanged,
+    );
+  }
+}
